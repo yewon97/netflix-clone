f80e200b19e1c863bc8dfb479c8b7b172e992839
yewon97 <dpdnjs9936@gmail.com>
2022-01-28 16:42:41 +0900

    로그인 스크린 디자인 완성

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginScreen from './screens/LoginScreen';
 
 function App() {
-  // const user = null; /* 로그인 안했을 때 */
-  const user = { /* 로그인 했을 때 */
-    name : 'sunny'
-  }
+  const user = null; /* 로그인 안했을 때 */
+  // const user = { /* 로그인 했을 때 */ name: 'sunny' };
   return (
     <div className="app">
-    <Router>
-      {!user ? (<LoginScreen />) : (
-      <Routes>
-        <Route path="/" exact element={<HomeScreen />} /> {/* exact prop는 경로를 정확히 매칭하고자 */}
-      </Routes>
-      )}
-    </Router>
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route path="/" exact element={<HomeScreen />} /> {/* exact prop는 경로를 정확히 매칭하고자 */}
+          </Routes>
+        )}
+      </Router>
     </div>
   );
 }
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import './LoginScreen.css'
+import './LoginScreen.css';
 
 function LoginScreen() {
   return (
-    <div className='loginScreen'>
-      
+    <div className="loginScreen">
+      <div className="loginScreen__background">
+        <img className="loginScreen__logo" src="images/login_logo.png" alt="" />
+        <button className="loginScreen__button">Sign In</button>
+
+        <div className="loginScreen__gradient" />
+      </div>
+
+      <div className="loginScreen__body">
+        <>
+          <h1>Unlimited films, TV programmes and more.</h1>
+          <h2>Watch anywhere, Cancel at any time.</h2>
+          <h3>Ready to watch? Enter your email to create or restart your memebership.</h3>
+
+          <div className="loginScreen__input">
+            <form>
+              <input type="email" placeholder="Eamil Address" />
+              <button className="loginScreen__getStarted">GET STARTED</button>
+            </form>
+          </div>
+        </>
+      </div>
     </div>
   );
 }
