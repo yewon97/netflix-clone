7698577505d8df0f298c7cf3e8a920ac80c9c3c6
yewon97 <dpdnjs9936@gmail.com>
2022-01-28 14:44:50 +0900

    homesreen, nav, banner, row, axios...

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCYt3m8yChFS_SfiqBMRA7lCyGT0vReo84",
+  authDomain: "netflix-clone-836d9.firebaseapp.com",
+  projectId: "netflix-clone-836d9",
+  storageBucket: "netflix-clone-836d9.appspot.com",
+  messagingSenderId: "530157468762",
+  appId: "1:530157468762:web:69e7c14d80cbf7c3fcac63"
+};
