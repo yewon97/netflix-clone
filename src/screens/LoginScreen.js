69d9dc1acbf92fa9ea609fd2c3920722bc99d948
yewon97 <dpdnjs9936@gmail.com>
2022-01-28 17:20:30 +0900

    onClick 이벤트 생성 -> 회원가입 창 만들 예정

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,29 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginScreen.css';
 
 function LoginScreen() {
+  const [singIn, setSingIn] = useState(false);
+
   return (
     <div className="loginScreen">
       <div className="loginScreen__background">
         <img className="loginScreen__logo" src="images/login_logo.png" alt="" />
-        <button className="loginScreen__button">Sign In</button>
+        <button onClick={() => {setSingIn(true)}} className="loginScreen__button">Sign In</button>
 
         <div className="loginScreen__gradient" />
       </div>
 
       <div className="loginScreen__body">
-        <>
-          <h1>Unlimited films, TV programmes and more.</h1>
-          <h2>Watch anywhere, Cancel at any time.</h2>
-          <h3>Ready to watch? Enter your email to create or restart your memebership.</h3>
+        {singIn ? (
+          <SignInScreen />
+        ) : (
+          <>
+            <h1>Unlimited films, TV programmes and more.</h1>
+            <h2>Watch anywhere, Cancel at any time.</h2>
+            <h3>Ready to watch? Enter your email to create or restart your memebership.</h3>
 
-          <div className="loginScreen__input">
-            <form>
-              <input type="email" placeholder="Eamil Address" />
-              <button className="loginScreen__getStarted">GET STARTED</button>
-            </form>
-          </div>
-        </>
+            <div className="loginScreen__input">
+              <form>
+                <input type="email" placeholder="Eamil Address" />
+                <button onClick={() => {setSingIn(true)}} className="loginScreen__getStarted">GET STARTED</button>
+              </form>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
