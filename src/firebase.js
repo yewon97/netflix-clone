5aa2bdb472d8d438098336479db187e5e979d2e0
yewon97 <dpdnjs9936@gmail.com>
2022-01-28 18:35:35 +0900

    firebase 회원가입 구현, firebase.js 안 import 경로 수정

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,19 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCYt3m8yChFS_SfiqBMRA7lCyGT0vReo84",
-  authDomain: "netflix-clone-836d9.firebaseapp.com",
-  projectId: "netflix-clone-836d9",
-  storageBucket: "netflix-clone-836d9.appspot.com",
-  messagingSenderId: "530157468762",
-  appId: "1:530157468762:web:69e7c14d80cbf7c3fcac63"
+  apiKey: 'AIzaSyCYt3m8yChFS_SfiqBMRA7lCyGT0vReo84',
+  authDomain: 'netflix-clone-836d9.firebaseapp.com',
+  projectId: 'netflix-clone-836d9',
+  storageBucket: 'netflix-clone-836d9.appspot.com',
+  messagingSenderId: '530157468762',
+  appId: '1:530157468762:web:69e7c14d80cbf7c3fcac63',
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db; /* default는 1개만 가능 */
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { auth } from '../firebase';
 import './SignUpScreen.css';
 
 function SignUpScreen() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
   const register = (e) => {
     e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(emailRef.current.value, passwordRef.current.value)
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
   const signIn = (e) => {
     e.preventDefault();
-  }
+  };
 
   return (
     <div className="signupScreen">
       <form>
         <h1>Sign In</h1>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button type="submit" onClick={signIn}>Sign In</button>
+        <input ref={emailRef} type="email" placeholder="Email" />
+        <input ref={passwordRef} type="password" placeholder="Password" />
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
 
         <h4>
-          <span className='signUpScreen__grey'>New to Netflix? </span>
-          <span className='signUpScreen__link' onClick={register}>Sign Up now.</span>
+          <span className="signUpScreen__grey">New to Netflix? </span>
+          <span className="signUpScreen__link" onClick={register}>
+            Sign Up now.
+          </span>
         </h4>
       </form>
     </div>
