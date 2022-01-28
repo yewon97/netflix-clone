57d3580a29ed9837f2cd210da215a96e0239a7bf
yewon97 <dpdnjs9936@gmail.com>
2022-01-28 15:59:17 +0900

    로그인 스크린 만들예정 - app.js에 로그인 조건 생성

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
+  // const user = null; /* 로그인 안했을 때 */
+  const user = { /* 로그인 했을 때 */
+    name : 'sunny'
+  }
   return (
     <div className="app">
     <Router>
+      {!user ? (<LoginScreen />) : (
       <Routes>
         <Route path="/" exact element={<HomeScreen />} /> {/* exact prop는 경로를 정확히 매칭하고자 */}
-        <Route path="/test" element={<h1>hi</h1>} />
       </Routes>
+      )}
     </Router>
     </div>
   );
