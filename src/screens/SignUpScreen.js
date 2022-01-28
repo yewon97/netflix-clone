78e21c8daba9b69470d2ad531e1a35c1bb2d8980
yewon97 <dpdnjs9936@gmail.com>
2022-01-28 18:50:01 +0900

    firebase로 회원가입, 로그인 구현

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -21,6 +21,15 @@ function SignUpScreen() {
 
   const signIn = (e) => {
     e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(emailRef.current.value, passwordRef.current.value)
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
   return (
