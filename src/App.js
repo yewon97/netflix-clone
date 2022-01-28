c970086b92eca7b24b418ee78c8c9e11900f4393
yewon97 <dpdnjs9936@gmail.com>
2022-01-28 19:12:14 +0900

    app.js 수정 -> unsubscribe 로그인, 로그아웃 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginScreen from './screens/LoginScreen';
+import { auth } from './firebase';
 
 function App() {
   const user = null; /* 로그인 안했을 때 */
   // const user = { /* 로그인 했을 때 */ name: 'sunny' };
+
+  useEffect(() => {
+    const unsubscrible = auth.onAuthStateChanged(userAuth => {
+      if(userAuth) {
+        // Logged in
+        console.log(userAuth);
+      } else {
+        // Logged out
+      }
+    });
+
+    return unsubscrible;
+  }, [])
+
   return (
     <div className="app">
       <Router>
