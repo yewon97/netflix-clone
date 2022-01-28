af7cd9a1b89f9e4bcc3361d0fd933ce6c185e504
yewon97 <dpdnjs9936@gmail.com>
2022-01-28 15:18:35 +0900

    public폴더에 이미지 폴더 생성, 로고, 아바타 이미지 경로 수정

--- src/Nav.js
+++ src/Nav.js
@@ -22,12 +22,12 @@ function Nav() {
       <div className="nav__contents">
         <img
           className='nav__logo'
-          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          src="images/logo.png"
           alt="netflix_logo"
         />
         <img
           className='nav__avatar'
-          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          src="images/avatar.png"
           alt="netflix_avatar"
         />
       </div>
